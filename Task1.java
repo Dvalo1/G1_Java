93444fbbd6af43da98ed078aa1b6895f7f274a70
Dvalo1 <32309229+Dvalo1@users.noreply.github.com>
2019-09-23 18:56:43 +0400

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,8 @@
+public class Task1 {
+    public static void main(String[] args) {
+        String[] languages = {"C++", "C#", "Java", "Pascal", "PHP", "JavaScript", "ActionScript"};
+        for (String i : languages) {
+            System.out.println(i);
+        }
+    }
+}
