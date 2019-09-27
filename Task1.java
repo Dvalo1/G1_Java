451f4bee23b785ad1a41174ab46665e28dcd3e49
student <student@BTU.local>
2019-09-27 10:08:45 +0400

    Week 3 Project

--- Task1.java
+++
@@ -1,8 +0,0 @@
-public class Task1 {
-    public static void main(String[] args) {
-        String[] languages = {"C++", "C#", "Java", "Pascal", "PHP", "JavaScript", "ActionScript"};
-        for (String i : languages) {
-            System.out.println(i);
-        }
-    }
-}
---
+++ Week3_1/src/BTU_company/Main.java
@@ -0,0 +1,10 @@
+package BTU_company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        int x = 12;
+        System.out.println(x+100);
+    }
+}
